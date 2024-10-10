c08597a818e372d74c6a149308bd0609ee5c23f8
Richard Kingston <richard.kingston@nordcloud.com>
2024-10-10 18:06:07 +0100

    Add Private DNS Resolver

--- infrastructure/modules/private-dns-zone-resolver/output.tf
+++ infrastructure/modules/private-dns-zone-resolver/output.tf
@@ -2,6 +2,6 @@ output "name" {
   value = azurerm_private_dns_resolver.private_dns_resolver.name
 }
 
-output "inbound_endpoint_ip_address" {
-  value = azurerm_private_dns_resolver_inbound_endpoint.private_dns_resolver_inbound_endpoint[0].private_ip_address
+output "private_dns_resolver_ip" {
+  value = azurerm_private_dns_resolver_inbound_endpoint.private_dns_resolver_inbound_endpoint.ip_configurations.0.private_ip_address
 }
