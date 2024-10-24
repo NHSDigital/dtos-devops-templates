9ebc90eba8a2db21ed9ae716ff58e733541dbb0f
Richard Kingston <richard.kingston@nordcloud.com>
2024-10-24 18:58:07 +0100

    Add Storage RBAC

--- infrastructure/modules/storage/rbac.tf
+++ infrastructure/modules/storage/rbac.tf
@@ -5,7 +5,7 @@ module "rbac_assignmnents" {
   source = "git::https://github.com/NHSDigital/dtos-devops-templates.git//infrastructure/modules/rbac-assignment?ref=88d5befe18207895d1dd09ec514c6f2f9b1379b2"
 
   principal_id         = data.azurerm_client_config.current
-  role_definition_name = each.value.role_definition_name
+  role_definition_name = each.value
   scope                = azurerm_storage_account.storage_account.id
 }
 
