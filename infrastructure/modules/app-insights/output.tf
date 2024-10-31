c0074a8f76e0f960ad224ed0224be576915d9aaa
patrickmoore-nc <94625903+patrickmoore-nc@users.noreply.github.com>
2024-10-31 13:13:11 +0000

    feat: DTOSS-4810 centralise audit modules

---
+++ infrastructure/modules/app-insights/output.tf
@@ -0,0 +1,8 @@
+
+output "connection_string" {
+  value = azurerm_application_insights.appins.connection_string
+}
+
+output "id" {
+  value = azurerm_application_insights.appins.id
+}
--- infrastructure/modules/function-app/output.tf
+++ infrastructure/modules/function-app/output.tf
@@ -3,6 +3,11 @@ output "name" {
   value       = azurerm_linux_function_app.function_app.name
 }
 
+output "id" {
+  description = "The id of the Linux Function App."
+  value       = azurerm_linux_function_app.function_app.id
+}
+
 output "function_app_sami_id" {
   description = "The Principal ID of the System Assigned Managed Service Identity that is configured on this Linux Function App."
   value       = azurerm_linux_function_app.function_app.identity.0.principal_id
