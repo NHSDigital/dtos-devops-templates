2a4236de903af394a0a88e6e3dffb1658299ee89
patrickmoore-nc <94625903+patrickmoore-nc@users.noreply.github.com>
2024-11-14 15:21:38 +0000

    fix/expanded lets encrypt certificate output attributes (#49)

--- infrastructure/modules/lets-encrypt-certificate/output.tf
+++ infrastructure/modules/lets-encrypt-certificate/output.tf
@@ -2,10 +2,12 @@
 output "key_vault_certificates" {
   value = {
     for k, v in local.letsencrypt_certs_map : k => {
-      name           = v.cert_key
-      subject        = v.cert_subject
-      location       = v.region
-      id             = data.azurerm_key_vault_certificate.letsencrypt[k].id
+      name                  = v.cert_key
+      subject               = v.cert_subject
+      location              = v.region
+      id                    = data.azurerm_key_vault_certificate.letsencrypt[k].id
+      versionless_id        = data.azurerm_key_vault_certificate.letsencrypt[k].versionless_id
+      versionless_secret_id = data.azurerm_key_vault_certificate.letsencrypt[k].versionless_secret_id
     }
   }
 }
