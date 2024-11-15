45bd87ff4dd05efcb9cdd88c73e4db93a498390b
fahadbaigNC <fahad.baig@nordcloud.com>
2024-11-15 12:11:29 +0000

    feat DTOS 5205 changes to add diagnostic settings to sqlserver

--- infrastructure/modules/sql-server/variables.tf
+++ infrastructure/modules/sql-server/variables.tf
@@ -186,7 +186,7 @@ variable "primary_blob_endpoint_name" {
   description = "name of storage account primary endpoint"
 }
 
-variable "retention_in_days     " {
+variable "retention_in_days" {
   type        = number
   description = "number of days for audit log policies"
 }
