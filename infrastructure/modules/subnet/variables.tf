d26d45b4435b7cc650bf894151721818d3f07a21
fahadbaigNC <fahad.baig@nordcloud.com>
2024-11-19 17:15:22 +0000

    feat: DTOS 5205 rollout of DS for appservice, keyvault and networking

--- infrastructure/modules/subnet/variables.tf
+++ infrastructure/modules/subnet/variables.tf
@@ -36,6 +36,21 @@ variable "network_security_group_name" {
   description = "The name of the network security group."
 }
 
+variable "log_analytics_workspace_id" {
+  type        = string
+  description = "value of the log analytics workspace id"
+}
+
+variable "monitor_diagnostic_setting_network_security_group_enabled_logs" {
+  type        = list(string)
+  description = "Controls what logs will be enabled for the NSG"
+}
+
+variable "monitor_diagnostic_setting_network_security_group_metrics" {
+  type        = list(string)
+  description = "Controls what metrics will be enabled for the NSG"
+}
+
 variable "network_security_group_nsg_rules" {
   description = "The network security group rules."
   default     = []
