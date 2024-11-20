54d92e7d41980c9528add43db41c6a0e11bfc4f0
josielsouzanordcloud <90701149+josielsouzanordcloud@users.noreply.github.com>
2024-11-20 14:42:06 +0000

    Add public IP address to the template (#56)
    * Add public IP address to the template
    
    * Adding public IP address to APIM module

--- infrastructure/modules/api-management/main.tf
+++ infrastructure/modules/api-management/main.tf
@@ -1,9 +1,10 @@
 resource "azurerm_api_management" "apim" {
-  name                = var.name
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  publisher_name      = var.publisher_name
-  publisher_email     = var.publisher_email
+  name                 = var.name
+  location             = var.location
+  resource_group_name  = var.resource_group_name
+  publisher_name       = var.publisher_name
+  publisher_email      = var.publisher_email
+  public_ip_address_id = var.public_ip_address_id
 
   sku_name = "${var.sku_name}_${var.sku_capacity}"
   zones    = var.zones
