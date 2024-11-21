9d73d549233d4ee04ab1d24f6dc33bce9db32bd5
patrickmoore-nc <94625903+patrickmoore-nc@users.noreply.github.com>
2024-11-21 11:51:21 +0000

    fix: module name spelling in calls to subordinate RBAC Assignment module

--- infrastructure/modules/function-app/rbac.tf
+++ infrastructure/modules/function-app/rbac.tf
@@ -1,4 +1,4 @@
-module "rbac_assignmnents" {
+module "rbac_assignments" {
   # This results in a numbered index, necessary to avoid keying on values that are only known after apply (scope is a resource id)
   for_each = { for idx, assignment in var.rbac_role_assignments : idx => assignment }
 
