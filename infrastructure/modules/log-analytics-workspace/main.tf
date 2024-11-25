a947f5fcab997165e98fea22731b1aaa8c81afd2
Richard Kingston <98461689+rfk-nc@users.noreply.github.com>
2024-11-25 10:01:23 +0000

    feat: DTOS 5205 law changes of value source (#66)
    Co-authored-by: fahadbaigNC <fahad.baig@nordcloud.com>

--- infrastructure/modules/log-analytics-workspace/main.tf
+++ infrastructure/modules/log-analytics-workspace/main.tf
@@ -17,7 +17,8 @@ module "diagnostic-settings" {
 
   name                       = "${var.name}-diagnostic-setting"
   target_resource_id         = azurerm_log_analytics_workspace.log_analytics_workspace.id
-  log_analytics_workspace_id = var.log_analytics_workspace_id
+
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.log_analytics_workspace.id
   enabled_log                = var.monitor_diagnostic_setting_log_analytics_workspace_enabled_logs
   metric                     = var.monitor_diagnostic_setting_log_analytics_workspace_metrics
 
