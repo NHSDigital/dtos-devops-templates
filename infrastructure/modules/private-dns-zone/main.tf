d7e0176dc4958ab76343f40178565d3dec9da3dc
patrickmoore-nc <94625903+patrickmoore-nc@users.noreply.github.com>
2024-11-26 12:44:05 +0000

    Fix: Private DNS zone link (#70)
    This had been mistakenly made conditional to apply only for privatelink zones.

--- infrastructure/modules/private-dns-zone/main.tf
+++ infrastructure/modules/private-dns-zone/main.tf
@@ -6,8 +6,6 @@ resource "azurerm_private_dns_zone" "private_dns_zone" {
 }
 
 resource "azurerm_private_dns_zone_virtual_network_link" "private_dns_zone_vnet_link" {
-  count = strcontains(var.name, "privatelink") ? 1 : 0
-
   name                  = var.name
   resource_group_name   = var.resource_group_name
   private_dns_zone_name = azurerm_private_dns_zone.private_dns_zone.name
