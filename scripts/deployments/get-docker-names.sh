90620805cd51cd1860480a3ba377ff288a0d3995
patrickmoore-nc <94625903+patrickmoore-nc@users.noreply.github.com>
2024-12-06 11:40:37 +0000

    fix: better matching of shared code folder in CI image build

--- scripts/deployments/get-docker-names.sh
+++ scripts/deployments/get-docker-names.sh
@@ -34,7 +34,7 @@ changed_functions=""
 if [ -z "$CHANGED_FOLDERS" ]; then
     changed_functions="null"
     echo "No files changed"
-elif [[ "$CHANGED_FOLDERS" == *Shared* ]]; then
+elif [[ "$CHANGED_FOLDERS" =~ (?i)Shared ]]; then
     echo "Shared folder changed, returning all functions"
     for key in "${!docker_functions_map[@]}"; do
         changed_functions+=" ${docker_functions_map[$key]}"
