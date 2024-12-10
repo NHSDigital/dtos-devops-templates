97feece4ec58090bcd87d5d0f5bceb7485dace38
Alastair Lock <alastair.lock@gmail.com>
2024-12-10 23:10:06 +0000

    add in extra output from function app module

--- infrastructure/modules/function-app/output.tf
+++ infrastructure/modules/function-app/output.tf
@@ -3,6 +3,11 @@ output "name" {
   value       = azurerm_linux_function_app.function_app.name
 }
 
+output "function_app_endpoint_name" {
+  description = "The function app endpoint name."
+  value       = var.function_app_name
+}
+
 output "id" {
   description = "The id of the Linux Function App."
   value       = azurerm_linux_function_app.function_app.id
