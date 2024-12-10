31cdc971d771683ca6829d28517819b6b2bd451e
fahadbaigNC <103640920+fahadbaigNC@users.noreply.github.com>
2024-12-10 12:03:34 +0000

    feat: DTOS 00000 variable skip service principal aad check defaulted to false (#81)

--- infrastructure/modules/rbac-assignment/main.tf
+++ infrastructure/modules/rbac-assignment/main.tf
@@ -1,8 +1,9 @@
 # Note it is not necessary to declare the name for the role assignment as this is auto-generated in GUID format.
 resource "azurerm_role_assignment" "role_assignment" {
-  scope              = var.scope
-  principal_id       = var.principal_id
-  role_definition_id = data.azurerm_role_definition.role_definition.id
+  scope                            = var.scope
+  principal_id                     = var.principal_id
+  role_definition_id               = data.azurerm_role_definition.role_definition.id
+  skip_service_principal_aad_check = var.skip_service_principal_aad_check
 }
 
 # Declare the azurerm_subscription data source as this is required to get the full path for the role definition
