f9a7c4aa2ac8595ea0e4d880e25a0bd5f5592b38
Nimmo <dnimmo@gmail.com>
2024-12-16 12:45:21 +0000

    add prevent_destroy to resource (#85)

--- infrastructure/modules/postgresql-flexible/database.tf
+++ infrastructure/modules/postgresql-flexible/database.tf
@@ -5,4 +5,8 @@ resource "azurerm_postgresql_flexible_server_database" "postgresql_flexible_db" 
   server_id = azurerm_postgresql_flexible_server.postgresql_flexible_server.id
   charset   = each.value.charset
   collation = each.value.collation
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
