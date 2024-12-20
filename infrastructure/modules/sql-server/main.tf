946e264d144c5b66704e68c08f3376003fd6ab14
Alastair Lock <alastair.lock@gmail.com>
2024-12-20 16:48:25 +0000

    wip

--- infrastructure/modules/sql-server/main.tf
+++ infrastructure/modules/sql-server/main.tf
@@ -68,16 +68,16 @@ module "private_endpoint_sql_server" {
 /* --------------------------------------------------------------------------------------------------
   SQL Server Diagnostic Settings
 -------------------------------------------------------------------------------------------------- */
-module "diagnostic_setting_sql_server" {
+# module "diagnostic_setting_sql_server" {
 
-  source = "../diagnostic-settings"
+#   source = "../diagnostic-settings"
 
-  name                       = "${var.name}-sql-server-diagnotic-setting"
-  target_resource_id         = "${azurerm_mssql_server.azure_sql_server.id}/databases/master"
-  log_analytics_workspace_id = var.log_analytics_workspace_id
-  enabled_log                = var.monitor_diagnostic_setting_sql_server_enabled_logs
-  metric                     = var.monitor_diagnostic_setting_sql_server_metrics
-}
+#   name                       = "${var.name}-sql-server-diagnotic-setting"
+#   target_resource_id         = "${azurerm_mssql_server.azure_sql_server.id}/databases/master"
+#   log_analytics_workspace_id = var.log_analytics_workspace_id
+#   enabled_log                = var.monitor_diagnostic_setting_sql_server_enabled_logs
+#   metric                     = var.monitor_diagnostic_setting_sql_server_metrics
+# }
 
 /* --------------------------------------------------------------------------------------------------
   SQL Server Extended Auditing Policy
