78e27f20e1b3bbd24e2becb7c5b8ca4c2fc7cb10
Alastair Lock <alastair.lock@gmail.com>
2025-01-02 16:23:20 +0000

    Add private endpoint properties

--- infrastructure/modules/event-grid-topic/main.tf
+++ infrastructure/modules/event-grid-topic/main.tf
@@ -17,3 +17,32 @@ resource "azurerm_eventgrid_topic" "azurerm_eventgrid" {
 
   tags = var.tags
 }
+
+/* --------------------------------------------------------------------------------------------------
+  Private Endpoint Configuration
+-------------------------------------------------------------------------------------------------- */
+
+module "private_endpoint" {
+  count = var.private_endpoint_properties.private_endpoint_enabled ? 1 : 0
+
+  source = "../private-endpoint"
+
+  name                = "${var.function_app_name}-private-endpoint"
+  resource_group_name = var.private_endpoint_properties.private_endpoint_resource_group_name
+  location            = var.location
+  subnet_id           = var.private_endpoint_properties.private_endpoint_subnet_id
+
+  private_dns_zone_group = {
+    name                 = "${var.function_app_name}-private-endpoint-zone-group"
+    private_dns_zone_ids = var.private_endpoint_properties.private_dns_zone_ids
+  }
+
+  private_service_connection = {
+    name                           = "${var.function_app_name}-private-endpoint-connection"
+    private_connection_resource_id = azurerm_linux_function_app.function_app.id
+    subresource_names              = ["sites"]
+    is_manual_connection           = var.private_endpoint_properties.private_service_connection_is_manual
+  }
+
+  tags = var.tags
+}
