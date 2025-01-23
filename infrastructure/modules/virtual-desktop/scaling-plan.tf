76cc910aa60743f1d33812117b70619ae7ed2315
patrickmoore-nc <94625903+patrickmoore-nc@users.noreply.github.com>
2025-01-23 00:12:50 +0000

    feat: AVD host pool scaling (#96)
    * feat: AVD host pool scaling
    
    * wip
    
    * wip
    
    * wip
    
    * remove RBAC assignment, do it manually
    
    * remove RBAC assignment, do it manually

---
+++ infrastructure/modules/virtual-desktop/scaling-plan.tf
@@ -0,0 +1,35 @@
+# Pre-requisite: RBAC assignment at subscription level of role 'Desktop Virtualization Power On Off Contributor' for Service Principal 'Windows Virtual Desktop'
+# https://learn.microsoft.com/en-gb/azure/virtual-desktop/autoscale-create-assign-scaling-plan?tabs=portal%2Cintune&pivots=power-management#create-a-custom-rbac-role
+
+resource "azurerm_virtual_desktop_scaling_plan" "this" {
+  name                = var.scaling_plan_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  time_zone           = "GMT Standard Time"
+
+  schedule {
+    name                                 = "Weekdays"
+    days_of_week                         = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+    ramp_up_start_time                   = "07:00"
+    ramp_up_load_balancing_algorithm     = "BreadthFirst"
+    ramp_up_minimum_hosts_percent        = floor(100 / var.vm_count) # a single session host
+    ramp_up_capacity_threshold_percent   = 60
+    peak_start_time                      = "09:00"
+    peak_load_balancing_algorithm        = "BreadthFirst"
+    ramp_down_start_time                 = "18:00"
+    ramp_down_load_balancing_algorithm   = "DepthFirst"
+    ramp_down_minimum_hosts_percent      = floor(100 / var.vm_count) # a single session host
+    ramp_down_force_logoff_users         = true
+    ramp_down_wait_time_minutes          = 15
+    ramp_down_notification_message       = "You will be logged off in 15 mins. Please make sure to save your work."
+    ramp_down_capacity_threshold_percent = 80
+    ramp_down_stop_hosts_when            = "ZeroActiveSessions"
+    off_peak_start_time                  = "19:00"
+    off_peak_load_balancing_algorithm    = "DepthFirst"
+  }
+
+  host_pool {
+    hostpool_id          = azurerm_virtual_desktop_host_pool.this.id
+    scaling_plan_enabled = true
+  }
+}
