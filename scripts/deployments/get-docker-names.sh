921da43235978d783003f8912ac273a654577e27
Maciej Murawski <maciej.murawski@nordcloud.com>
2025-03-03 13:46:42 +0000

    fix: concatenating the outputs from multiple compose files

--- scripts/deployments/get-docker-names.sh
+++ scripts/deployments/get-docker-names.sh
@@ -69,7 +69,7 @@ changed_functions_json=$(printf '["%s"]' "$(echo "${changed_functions}" | sed 's
 # echo "Final list of functions to rebuild:"
 # echo ${changed_functions_json}
 
-echo "${changed_functions_json}" >> "${GITHUB_OUTPUT}"
+echo "${changed_functions_json}"
 #echo "local_func_names=$changed_functions_json" >> "$GITHUB_OUTPUT"
 
 
