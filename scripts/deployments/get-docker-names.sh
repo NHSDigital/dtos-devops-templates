122f683961b849bf882ce418dd7a99008378b26f
patrickmoore-nc <94625903+patrickmoore-nc@users.noreply.github.com>
2025-03-05 07:49:50 +0000

    fix: DTOSS-7756 track file changes in nested folders for image rebuilds (#114)

--- scripts/deployments/get-docker-names.sh
+++ scripts/deployments/get-docker-names.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 if [ $# -lt 2 ]; then
-  echo "Usage: $0 <docker_compose_file> <excluded_containers_comma_separated>"
-  exit 1
+    echo "Usage: $0 <docker_compose_file> <excluded_containers_comma_separated>"
+    exit 1
 fi
-
+echo "Parsing Docker compose file..."
+echo
 DOCKER_COMPOSE_FILE=$1
 WORKING_DIR="$(dirname "$1")"
 EXCLUDED_CONTAINERS=$2
@@ -15,19 +16,29 @@ cd "${WORKING_DIR}" || { echo "Directory not found: ${WORKING_DIR}"; exit 1; }
 
 declare -A docker_functions_map=()
 
-set -x
-
 for service in $(yq eval ".services[] | select($EXCLUSION_FILTER) | .container_name" ${DOCKER_COMPOSE_FILE}); do
-  context=$(yq eval ".services[] | select(.container_name == \"$service\") | .build.context" ${DOCKER_COMPOSE_FILE})
-  dockerfile=$(yq eval ".services[] | select(.container_name == \"$service\") | .build.dockerfile" ${DOCKER_COMPOSE_FILE})
+    context=$(yq eval ".services[] | select(.container_name == \"$service\") | .build.context" ${DOCKER_COMPOSE_FILE})
+    dockerfile=$(yq eval ".services[] | select(.container_name == \"$service\") | .build.dockerfile" ${DOCKER_COMPOSE_FILE})
 
-  if [ -z ${dockerfile} ] || [ -z ${context} ] ; then
-    continue
-  fi
-  contextFiltered=$(echo ${context} | sed 's#^\./src/##' | sed 's#^\./##' | sed 's#/$##')
-  dockerfileFiltered=$(echo ${dockerfile} | sed 's#^\./##' | sed 's#\/Dockerfile##' | sed 's#Dockerfile##' )
-  docker_functions_map[${contextFiltered}${dockerfileFiltered}]=${service}
+    if [ -z ${dockerfile} ] || [ -z ${context} ]; then
+        continue
+    fi
+    context_filtered=$(echo ${context} | sed 's#^\./src/##' | sed 's#^\./##' | sed 's#/$##')
+    dockerfile_filtered=$(echo ${dockerfile} | sed 's#^\./##' | sed 's#\/Dockerfile##' | sed 's#Dockerfile##')
+    if [ -n "${context_filtered}" ] && [ -n "${dockerfile_filtered}" ]; then
+        function_path="${context_filtered}/${dockerfile_filtered}"
+    else
+        function_path="${context_filtered}${dockerfile_filtered}"
+    fi
+    docker_functions_map[${function_path}]=${service}
 done
+
+printf "%-40s %-50s\n" "Service" "Path"
+printf "%-40s %-50s\n" "-------" "----"
+for key in "${!docker_functions_map[@]}"; do
+    printf "%-40s %-50s\n" "${docker_functions_map[$key]}" "$key"
+done
+echo
 
 changed_functions=""
 
@@ -35,24 +46,31 @@ if [ -z "${CHANGED_FOLDERS}" ]; then
     changed_functions="null"
     echo "No files changed"
 elif [[ "${CHANGED_FOLDERS,,}" =~ shared ]]; then
-    echo "Shared folder changed, returning all functions"
+    echo "Shared folder changed, building all images"
     for key in "${!docker_functions_map[@]}"; do
         changed_functions+=" ${docker_functions_map[$key]}"
-        echo "Adding in: ${docker_functions_map[$key]}"
+        echo "- ${docker_functions_map[$key]}"
     done
 else
-    echo "files changed ${CHANGED_FOLDERS} "
+    echo "Checking changed folders..."
     for folder in ${CHANGED_FOLDERS}; do
-      echo "Add this function in: ${folder} "
-      echo "Add this which maps to: ${docker_functions_map[$folder]} "
-      changed_functions+=" ${docker_functions_map[$folder]}"
+        for function_path in "${!docker_functions_map[@]}"; do
+            # The changed folder may be a deeper path than the Function path, so it must be matched this way around
+            if [[ "${folder}" =~ ${function_path} ]]; then
+                changed_functions+=" ${docker_functions_map[$function_path]}"
+                echo "${folder} matches service '${docker_functions_map[$function_path]}'"
+                continue
+            fi
+        done
     done
 fi
+echo
 
 # Format the output for the github matrix:
-changed_functions_json=$(printf '["%s"]' "$(echo ${changed_functions} | sed 's/ /","/g')")
+changed_functions=${changed_functions# }
+changed_functions_json="[${changed_functions// /,}]"
 
-echo "Final list of functions to rebuild:"
+echo "List of services to build:"
 echo "${changed_functions_json}"
 
 echo "FUNC_NAMES=${changed_functions_json}" >> "${GITHUB_OUTPUT}"
