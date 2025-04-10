7358875ff9e3da8b98633bc932bd385f457c6fa9
fahadbaigNC <fahad.baig@nordcloud.com>
2025-04-10 16:11:02 +0100

    feat: DTOS 8142 create terraform module for azure policy assignment and terraform recrusive corrections for formatting

--- infrastructure/modules/policy-assignment/main.tf
+++ infrastructure/modules/policy-assignment/main.tf
@@ -1,5 +1,7 @@
+/* --------------------------------------------------------------------------------------------------
+   Subscription Policy Assignment
+-------------------------------------------------------------------------------------------------- */
 
-# Subscription Policy Assignment
 resource "azurerm_subscription_policy_assignment" "this" {
   count = var.subscription_id != null
 
@@ -17,7 +19,19 @@ resource "azurerm_subscription_policy_assignment" "this" {
   })
 }
 
-# Resource Group Policy Assignment
+resource "azurerm_subscription_policy_remediation" "this" {
+  count = var.subscription_id != null
+
+  name                 = var.name
+  subscription_id      = var.subscription_id
+  policy_assignment_id = azurerm_subscription_policy_assignment.this.id
+}
+
+
+/* --------------------------------------------------------------------------------------------------
+   Resource Group Policy Assignment
+-------------------------------------------------------------------------------------------------- */
+
 resource "azurerm_resource_group_policy_assignment" "this" {
   count = var.resource_group_id != null
 
@@ -26,11 +40,17 @@ resource "azurerm_resource_group_policy_assignment" "this" {
   resource_group_id    = var.resource_group_id
   enforce              = var.enforce
 
-
-
   parameters = jsonencode({
     log_analytics_workspace_id = {
       value = var.log_analytics_workspace_id
     }
   })
+}
+
+resource "azurerm_resource_group_policy_remediation" "this" {
+  count = var.resource_group_id != null
+
+  name                 = var.name
+  resource_group_id    = var.resource_group_id
+  policy_assignment_id = azurerm_resource_group_policy_assignment.this.id
 }
