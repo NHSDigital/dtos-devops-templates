cf283d935bd1c06b20dd9e05387d7e156d68ae69
fahadbaigNC <fahad.baig@nordcloud.com>
2025-04-10 12:53:29 +0100

    feat: DTOS 8142 create terraform module for azure policy assignment and terraform recrusive corrections for formatting

--- infrastructure/modules/policy-assignment/main.tf
+++ infrastructure/modules/policy-assignment/main.tf
@@ -1,35 +1,36 @@
-# Can it be fed in via tfvars instead of? data "azurerm_subscription" "current" {}
 
 # Subscription Policy Assignment
 resource "azurerm_subscription_policy_assignment" "this" {
+  count = var.subscription_id != null
+
   name                 = var.name
-  policy_definition_id = azurerm_policy_definition.this.id
+  policy_definition_id = var.policy_definition_id
   subscription_id      = var.subscription_id
   enforce              = var.enforce
 
-  non_compliance_messages = ["This resource is not compliant with the policy."]
+  non_compliance_messages = ["This resource is not compliant with policy ${var.policy_definition_id}."]
 
-  parameters = {
-    log_analytics_workspace_id = var.log_analytics_workspace_id
-  }
+  parameters = jsonencode({
+    log_analytics_workspace_id = {
+      value = var.log_analytics_workspace_id
+    }
+  })
 }
 
 # Resource Group Policy Assignment
 resource "azurerm_resource_group_policy_assignment" "this" {
+  count = var.resource_group_id != null
+
   name                 = var.name
-  resource_group_id    = azurerm_resource_group.this.id
-  policy_definition_id = azurerm_policy_definition.this.id
+  policy_definition_id = var.policy_definition_id
+  resource_group_id    = var.resource_group_id
   enforce              = var.enforce
 
-  parameters = {
-    log_analytics_workspace_id = var.log_analytics_workspace_id
-  }
+
+
+  parameters = jsonencode({
+    log_analytics_workspace_id = {
+      value = var.log_analytics_workspace_id
+    }
+  })
 }
-
-
-
-# count = var.vnet_integration_enabled ? 1 : 0
-
-# count = can(var.private_endpoint_properties.private_endpoint_enabled) ? 1 : 0
-
-# count = var.private_endpoint_properties.private_endpoint_enabled ? 1 : 0
--- infrastructure/modules/policy-assignment/variables.tf
+++ infrastructure/modules/policy-assignment/variables.tf
@@ -15,10 +15,10 @@ variable "log_analytics_workspace_id" {
   default     = null
 }
 
-
 variable "resource_group_id" {
   description = "The resource group ID for the resource group policy assignment."
   type        = string
+  default     = null
 }
 
 variable "policy_definition_id" {
@@ -29,41 +29,5 @@ variable "policy_definition_id" {
 variable "subscription_id" {
   description = "The subscription ID to which the policy will be assigned."
   type        = string
+  default     = null
 }
-
-
-
-# variable "enabled_log" {
-#   type        = list(string)
-#   description = "value of the enabled log"
-#   default     = []
-# }
-
-# variable "eventhub_authorization_rule_id" {
-#   type        = string
-#   description = "value of the eventhub authorization rule id"
-#   default     = null
-# }
-
-# variable "eventhub_name" {
-#   type        = string
-#   description = "value of the eventhub name if logging to eventhub is being used."
-#   default     = null
-# }
-
-# variable "metric" {
-#   type        = list(string)
-#   description = "value of the metric"
-#   default     = []
-# }
-
-# variable "metric_enabled" {
-#   type        = bool
-#   description = "to enable retention for diagnostic settings metric"
-#   default     = true
-# }
-
-# variable "target_resource_id" {
-#   type        = string
-#   description = "value of the target resource id"
-# }
