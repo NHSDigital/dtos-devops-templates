3b4f54452232876687bbd534d6c6bd8349648f83
fahadbaigNC <fahad.baig@nordcloud.com>
2025-04-10 16:40:45 +0100

    feat: DTOS 8142 create terraform module for azure policy assignment and terraform recrusive corrections for formatting

--- infrastructure/modules/policy-assignment/main.tf
+++ infrastructure/modules/policy-assignment/main.tf
@@ -3,7 +3,7 @@
 -------------------------------------------------------------------------------------------------- */
 
 resource "azurerm_subscription_policy_assignment" "this" {
-  count = var.subscription_id != null
+  count = var.subscription_id != null ? 1 : 0
 
   name                 = var.name
   policy_definition_id = var.policy_definition_id
@@ -20,7 +20,7 @@ resource "azurerm_subscription_policy_assignment" "this" {
 }
 
 resource "azurerm_subscription_policy_remediation" "this" {
-  count = var.subscription_id != null
+  count = var.subscription_id != null ? 1 : 0
 
   name                 = var.name
   subscription_id      = var.subscription_id
@@ -33,7 +33,7 @@ resource "azurerm_subscription_policy_remediation" "this" {
 -------------------------------------------------------------------------------------------------- */
 
 resource "azurerm_resource_group_policy_assignment" "this" {
-  count = var.resource_group_id != null
+  count = var.resource_group_id != null ? 1 : 0
 
   name                 = var.name
   policy_definition_id = var.policy_definition_id
@@ -48,7 +48,7 @@ resource "azurerm_resource_group_policy_assignment" "this" {
 }
 
 resource "azurerm_resource_group_policy_remediation" "this" {
-  count = var.resource_group_id != null
+  count = var.resource_group_id != null ? 1 : 0
 
   name                 = var.name
   resource_group_id    = var.resource_group_id
