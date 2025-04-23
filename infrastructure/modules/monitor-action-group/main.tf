a4b94bb0a55e47515407028031c350a48191c0f5
Alastair Lock <alastair.lock@gmail.com>
2025-04-23 11:53:56 +0100

    feat: DTOSS-8458: Create a terraform module for azure action group (#145)
    * DTOSS-8458: Create a Terraform module for Azure Action Group
    
    * DTOSS-8458: Create a Terraform module for Azure Action Group
    
    * Pr changes
    
    * Pr changes
    
    * Pr changes

---
+++ infrastructure/modules/monitor-action-group/main.tf
@@ -0,0 +1,53 @@
+resource "azurerm_monitor_action_group" "this" {
+  name                = var.name
+  resource_group_name = var.resource_group_name
+  short_name          = var.short_name
+
+  dynamic "email_receiver" {
+    for_each = var.email_receiver != null ? var.email_receiver : {}
+    content {
+      name                    = email_receiver.value.name
+      email_address           = email_receiver.value.email_address
+      use_common_alert_schema = email_receiver.value.use_common_alert_schema
+    }
+  }
+
+  dynamic "event_hub_receiver" {
+    for_each = var.event_hub_receiver != null ? var.event_hub_receiver : {}
+    content {
+      name                    = event_hub_receiver.value.name
+      event_hub_namespace     = event_hub_receiver.value.event_hub_namespace
+      event_hub_name          = event_hub_receiver.value.event_hub_name
+      subscription_id         = event_hub_receiver.value.subscription_id
+      use_common_alert_schema = event_hub_receiver.value.use_common_alert_schema
+    }
+  }
+
+  dynamic "sms_receiver" {
+    for_each = var.sms_receiver != null ? var.sms_receiver : {}
+    content {
+      name         = sms_receiver.value.name
+      country_code = sms_receiver.value.country_code
+      phone_number = sms_receiver.value.phone_number
+    }
+  }
+
+  dynamic "voice_receiver" {
+    for_each = var.voice_receiver != null ? var.voice_receiver : {}
+    content {
+      name         = voice_receiver.value.name
+      country_code = voice_receiver.value.country_code
+      phone_number = voice_receiver.value.phone_number
+    }
+  }
+
+  dynamic "webhook_receiver" {
+    for_each = var.webhook_receiver != null ? var.webhook_receiver : {}
+    content {
+      name                    = webhook_receiver.value.name
+      service_uri             = webhook_receiver.value.service_uri
+      use_common_alert_schema = webhook_receiver.value.use_common_alert_schema
+    }
+  }
+
+}
---
+++ infrastructure/modules/monitor-action-group/variables.tf
@@ -0,0 +1,81 @@
+variable "name" {
+  type        = string
+  description = "value of the name of the diagnostic setting"
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group in which to create the monitor action group."
+  type        = string
+}
+
+variable "short_name" {
+  description = "The short name of the action group. This will be used in SMS messages."
+  type        = string
+}
+
+variable "email_receiver" {
+  description = "Email receiver properties."
+  type = map(object({
+    name                    = string
+    email_address           = string
+    use_common_alert_schema = optional(bool, false)
+  }))
+  default = null
+}
+
+variable "event_hub_receiver" {
+  description = "event hub receiver properties."
+  type = map(object({
+    name                    = string
+    event_hub_namespace     = string
+    event_hub_name          = string
+    subscription_id         = string
+    use_common_alert_schema = bool
+  }))
+  default = null
+}
+
+variable "sms_receiver" {
+  description = "sms receiver properties."
+  type = map(object({
+    name         = string
+    country_code = string
+    phone_number = string
+  }))
+  default = null
+}
+
+variable "voice_receiver" {
+  description = "voice receiver properties."
+  type = map(object({
+    name         = string
+    country_code = string
+    phone_number = string
+  }))
+  default = null
+}
+
+variable "webhook_receiver" {
+  description = "webhook receiver properties."
+  type = map(object({
+    name                    = string
+    service_uri             = string
+    use_common_alert_schema = bool
+  }))
+  default = null
+}
+
+variable "location" {
+  description = "The location/region where the Event Hub namespace is created."
+  type        = string
+  validation {
+    condition     = contains(["uksouth", "ukwest"], var.location)
+    error_message = "The location must be either uksouth or ukwest."
+  }
+}
+
+variable "tags" {
+  description = "A mapping of tags to assign to the resource."
+  type        = map(string)
+  default     = {}
+}
