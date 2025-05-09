4026fecbf81c7439acdeef1fd1b6b6c55687753c
Colin Saliceti <8416694+saliceti@users.noreply.github.com>
2025-05-09 19:08:02 +0100

    wip

--- infrastructure/modules/container-app/data.tf
+++ infrastructure/modules/container-app/data.tf
@@ -8,6 +8,8 @@
 data "azurerm_key_vault_secrets" "app" {
   count = var.fetch_secrets_from_app_key_vault ? 1 : 0
 
+  depends_on = [module.key_vault_reader_role]
+
   key_vault_id = var.app_key_vault_id
 }
 
