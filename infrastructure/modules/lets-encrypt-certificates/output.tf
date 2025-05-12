0f1c4c653749b39e601ac6ee37021027433a1054
patrickmoore-nc <94625903+patrickmoore-nc@users.noreply.github.com>
2025-05-12 10:30:21 +0100

    fix: Document LetsEncrypt Certificates module and simplify outputs (#154)

--- infrastructure/modules/lets-encrypt-certificates/output.tf
+++ infrastructure/modules/lets-encrypt-certificates/output.tf
@@ -6,22 +6,10 @@ output "key_vault_certificates" {
       naming_key            = v.cert_key
       subject               = v.cert_subject
       location              = v.region
+      pfx_blob_secret_name  = "pfx-${data.azurerm_key_vault_certificate.letsencrypt[k].name}"
       id                    = data.azurerm_key_vault_certificate.letsencrypt[k].id
       versionless_id        = data.azurerm_key_vault_certificate.letsencrypt[k].versionless_id
       versionless_secret_id = data.azurerm_key_vault_certificate.letsencrypt[k].versionless_secret_id
-    }
-  }
-}
-
-output "key_vault_certificate_pfx_blobs" {
-  value = {
-    for k, v in local.letsencrypt_certs_map : k => {
-      name           = data.azurerm_key_vault_secret.pfx_blob[k].name
-      naming_key     = v.cert_key
-      subject        = v.cert_subject
-      location       = v.region
-      id             = data.azurerm_key_vault_secret.pfx_blob[k].id
-      versionless_id = data.azurerm_key_vault_secret.pfx_blob[k].versionless_id
     }
   }
 }
