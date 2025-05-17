7eb0247f8a9f193e6711a9da92e2fdc5d9f04f62
patrickmoore-nc <94625903+patrickmoore-nc@users.noreply.github.com>
2025-05-17 11:12:14 +0100

    acme-certificate module

--- infrastructure/modules/acme-certificate/providers.tf
+++ infrastructure/modules/acme-certificate/providers.tf
@@ -3,7 +3,7 @@ terraform {
     azurerm = {
       source = "hashicorp/azurerm"
       configuration_aliases = [
-        azurerm.dns.public,
+        azurerm.dns_public,
         azurerm.dns_private
       ]
     }
