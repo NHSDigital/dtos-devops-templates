ac0aaf63e6d27820912918e2ecd23feec518ce51
patrickmoore-nc <94625903+patrickmoore-nc@users.noreply.github.com>
2025-05-22 16:21:44 +0100

    feat: DTOSS-9032 app service plan zone balancing (#173)

--- infrastructure/modules/app-service-plan/main.tf
+++ infrastructure/modules/app-service-plan/main.tf
@@ -3,8 +3,9 @@ resource "azurerm_service_plan" "appserviceplan" {
   resource_group_name = var.resource_group_name
   location            = var.location
 
-  os_type  = var.os_type
-  sku_name = var.sku_name
+  os_type                = var.os_type
+  sku_name               = var.sku_name
+  zone_balancing_enabled = var.zone_balancing_enabled
 
   tags = var.tags
 
