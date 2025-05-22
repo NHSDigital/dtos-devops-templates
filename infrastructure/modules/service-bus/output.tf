a104892b6414325abf0697f92f16506375b96728
Alastair Lock <alastair.lock@gmail.com>
2025-05-22 12:25:38 +0100

    DTOSS-9003: Deploy service bus into the hub vnet

--- infrastructure/modules/service-bus/output.tf
+++ infrastructure/modules/service-bus/output.tf
@@ -3,12 +3,6 @@ output "servicebus_connection_string" {
   sensitive = true
 }
 
-# output "servicebus_topic_ids" {
-#   description = "A map of Service Bus Topic IDs"
-#   value       = { for k, topic in azurerm_servicebus_topic.this : k => topic.id }
-# }
-
-output "id" {
- description = "A map of Service Bus Topic IDs"
- value       = { for k, topic in azurerm_servicebus_topic.this : k => topic.id }
+output "namespace_id" {
+  value = azurerm_servicebus_namespace.this.id
 }
