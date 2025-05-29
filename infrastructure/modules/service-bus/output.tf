3205516383cf0a1f45f39e8fae61ecf81dfb2969
Alastair Lock <alastair.lock@gmail.com>
2025-05-29 11:11:30 +0100

    DTOSS-9226: Create Permissions for Foundry Function App

--- infrastructure/modules/service-bus/output.tf
+++ infrastructure/modules/service-bus/output.tf
@@ -7,6 +7,10 @@ output "namespace_id" {
   value = azurerm_servicebus_namespace.this.id
 }
 
+output "namespace_name" {
+  value = azurerm_servicebus_namespace.this.name
+}
+
 output "topic_ids" {
   value = { for k, topic in azurerm_servicebus_topic.this : k => topic.id }
 }
