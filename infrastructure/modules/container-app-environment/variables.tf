5f53ab8aef82cf38207e9ddfc5ab9ce37ba5c67f
Colin Saliceti <8416694+saliceti@users.noreply.github.com>
2025-06-25 15:12:47 +0100

    Set default value for workload_profile
    The caller does not have to specify the value and the individual default
    values in the object are used

--- infrastructure/modules/container-app-environment/variables.tf
+++ infrastructure/modules/container-app-environment/variables.tf
@@ -38,6 +38,7 @@ variable "workload_profile" {
     maximum_count         = optional(number, 1)
   })
   description = "Workload profile for the container app environment. This defines the scaling and resource allocation for the environment. Possible workload_profile_type values include Consumption, D4, D8, D16, D32, E4, E8, E16 and E32."
+  default     = {}
 }
 
 variable "zone_redundancy_enabled" {
