90cd9f7b3214b243c30735f58147778ed5d8c9df
Michael Justus <209924279+micjustus-nc@users.noreply.github.com>
2025-07-01 15:46:11 +0100

    Add new module to centralise rbac assignments to a single user assigned managed identity.

---
+++ infrastructure/modules/rbac-assignment-global/outputs.tf
@@ -0,0 +1,20 @@
+output "global_uami_id" {
+  value = azurerm_user_assigned_identity.global_uami.id
+}
+
+output "global_uami_principal_id" {
+  value = azurerm_user_assigned_identity.global_uami.principal_id
+}
+
+output "assigned_roles" {
+  description = "List of role assignments created."
+  value = [
+    for k, v in azurerm_role_assignment.this :
+    {
+      id                  = v.id
+      role_definition     = v.role_definition_name
+      scope               = v.scope
+      principal_id        = v.principal_id
+    }
+  ]
+}
