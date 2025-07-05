702c6868ed8952b9ba7751361e63ad2fa6be62b7
Michael Justus <209924279+micjustus-nc@users.noreply.github.com>
2025-07-05 21:35:56 +0100

    Test approach with role definitions

--- infrastructure/modules/managed-identity-global/main.tf
+++ infrastructure/modules/managed-identity-global/main.tf
@@ -2,72 +2,89 @@
 module "global_uami" {
   source = "../managed-identity"
 
-  uai_name            = join("-", [var.identity_prefix, var.environment])
+  uai_name            = join("-", compact([var.identity_prefix, var.environment]))
   location            = var.location
   resource_group_name = var.resource_group_name
   tags                = var.tags
 }
 
-resource "azurerm_role_assignment" "global_uami_role_assignments" {
-  for_each = {
-    for item in flatten([
-      for grp in local.grouped_role_assignments : [
-        for role in grp.roles : {
-          role  = role
-          scope = grp.scope
-        }
-      ]
-    ]) : "${item.scope}-${item.role}" => item
+resource "azurerm_role_definition" "global_uami_storage_role_definition" {
+  name        = "${var.identity_prefix}-${var.environment}-uami-global-storage-role"
+  scope       = data.azurerm_resource_group.target.id
+  description = "Contains the least required roles for storage access to queues, blobs and tables"
+
+  permissions {
+    actions = [
+      # Storage blob control-plane
+      "Microsoft.Storage/storageAccounts/blobServices/containers/read",
+      "Microsoft.Storage/storageAccounts/blobServices/generateUserDelegationKey/action"
+    ]
+    data_actions = [
+      # Storage blob data
+      "Microsoft.Storage/storageAccounts/blobServices/containers/blobs/read",
+
+      # Storage queue data
+      "Microsoft.Storage/storageAccounts/queueServices/queues/messages/add/action",
+      "Microsoft.Storage/storageAccounts/queueServices/queues/messages/read",
+      "Microsoft.Storage/storageAccounts/queueServices/queues/messages/process/action",
+
+      # Storage table data
+      "Microsoft.Storage/storageAccounts/tableServices/tables/entities/read",
+      "Microsoft.Storage/storageAccounts/tableServices/tables/entities/add/action",
+      "Microsoft.Storage/storageAccounts/tableServices/tables/entities/update/action",
+      "Microsoft.Storage/storageAccounts/tableServices/tables/entities/delete/action"
+    ]
+    not_actions       = []
+    not_data_actions  = []
   }
 
-  principal_id         = var.principal_id != null ? var.principal_id : module.global_uami.principal_id
-  role_definition_name = each.value.role
-  scope                = each.value.scope
-}
-
-locals {
-  default_roles = {
-    "keyvault" = [
-      "Key Vault Secrets User"
-    ]
-    "storage" = [
-      "Storage Blob Data Reader",
-      "Storage Table Data Contributor",
-      "Storage Queue Data Contributor"
-    ]
-    "sql" = [
-      "SQL DB Contributor"
-    ]
-  }
-
-  # Discover the type of the resource based on well-known ID patterns in Azure
-  resource_tuples = [
-    for rid in var.resource_ids : {
-      id = rid
-      type = (
-        can(regex("Microsoft.KeyVault/vaults", rid)) ? "keyvault" :
-        can(regex("Microsoft.Storage/storageAccounts", rid)) ? "storage" :
-        can(regex("Microsoft.Sql/servers", rid)) ? "sql" :
-        can(regex("^/subscriptions/[^/]+/resourceGroups/[^/]+$", rid)) ? "resource-group" :
-        can(regex("^/subscriptions/[^/]+$", rid)) ? "subscription" :
-        "unknown"
-      )
-    }
-    if rid != "" && rid != null
-  ]
-
-  scoped_role_map = {
-    for rt in local.resource_tuples :
-    rt.id => distinct(
-      lookup(var.custom_resource_roles, rt.type, lookup(local.default_roles, rt.type, []))
-    )
-  }
-
-  # We group by scope to reduce the number of assignments made
-  grouped_role_assignments = [
-    for scope, roles in local.scoped_role_map : {
-      scope = scope
-      roles = roles
-    }
+  assignable_scopes = [
+    data.azurerm_resource_group.target.id
   ]
 }
+
+resource "azurerm_role_definition" "global_uami_keyvault_role_definition" {
+  name        = "${var.identity_prefix}-${var.environment}-uami-global-keyvault-role"
+  scope       = data.azurerm_resource_group.target.id
+  description = "Contains the least required roles for keyvault access"
+
+  permissions {
+    actions = [
+    ]
+    data_actions = [
+      # Key Vault secrets
+      "Microsoft.KeyVault/vaults/secrets/getSecret/action",
+      "Microsoft.KeyVault/vaults/secrets/readMetadata/action",
+    ]
+    not_actions       = []
+    not_data_actions  = []
+  }
+
+  assignable_scopes = [
+    data.azurerm_resource_group.target.id
+  ]
+}
+
+resource "azurerm_role_definition" "global_uami_sql_role_definition" {
+  name        = "${var.identity_prefix}-${var.environment}-uami-global-sql-role"
+  scope       = data.azurerm_resource_group.target.id
+  description = "Contains the least required roles for SQL data access"
+
+  permissions {
+    actions = [
+      # SQL control-plane
+      "Microsoft.Sql/servers/*",
+      "Microsoft.Authorization/*/read",
+    ]
+    data_actions = [
+    ]
+    not_actions       = []
+    not_data_actions  = []
+  }
+
+  assignable_scopes = [
+    data.azurerm_resource_group.target.id
+  ]
+}
+
+
