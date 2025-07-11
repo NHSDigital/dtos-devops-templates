02f366aef04f791b1ad49a5226ece4eb8881c022
Josiel Souza <josiel.souza@nordcloud.com>
2025-07-11 15:16:02 +0100

    [DTOSS-9894] Add Azure AD authentication support to container app module
    Adds optional Entra ID authentication for the container app. Includes Key Vault integration for managing secrets and necessary Terraform configuration updates.

--- infrastructure/modules/container-app/data.tf
+++ infrastructure/modules/container-app/data.tf
@@ -1,6 +1,20 @@
+data "azurerm_client_config" "current" {}
 data "azurerm_key_vault_secrets" "app" {
   count      = var.fetch_secrets_from_app_key_vault ? 1 : 0
-  depends_on = [module.key_vault_reader_role]
+  depends_on = [module.key_vault_reader_role_app]
 
   key_vault_id = var.app_key_vault_id
+}
+
+data "azurerm_key_vault" "infra" {
+  provider            = azurerm.hub
+  count               = var.enable_auth ? 1 : 0
+  name                = var.infra_key_vault_name
+  resource_group_name = var.infra_key_vault_rg
+}
+
+data "azurerm_key_vault_secret" "infra" {
+  for_each     = var.enable_auth ? toset(var.infra_secret_names) : toset([])
+  name         = each.value
+  key_vault_id = data.azurerm_key_vault.infra[0].id
 }
