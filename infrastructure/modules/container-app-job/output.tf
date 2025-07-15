b0cfade92d70d0f6880b4d78bb93fe32eb4dfcb8
Colin Saliceti <8416694+saliceti@users.noreply.github.com>
2025-07-15 13:22:36 +0100

    wip

--- infrastructure/modules/container-app-job/output.tf
+++ infrastructure/modules/container-app-job/output.tf
@@ -1,10 +1,9 @@
 output "id" {
   description = "The ID of the container app job."
-  value       = azurerm_container_app_job.main.id
+  value       = azurerm_container_app_job.this.id
 }
 
 output "image" {
   description = "The image used by the container app job."
-  value       = azurerm_container_app_job.main.template[0].container[0].image
-
+  value       = azurerm_container_app_job.this.template[0].container[0].image
 }
