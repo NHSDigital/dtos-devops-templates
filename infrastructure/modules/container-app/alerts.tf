906d3b6dba8dc9d5cb7043a75ac35879b92483dc
Alastair Lock <alastair.lock@gmail.com>
2025-10-02 11:49:15 +0100

    remove the storage alert

--- infrastructure/modules/container-app/alerts.tf
+++ infrastructure/modules/container-app/alerts.tf
@@ -4,13 +4,13 @@ resource "azurerm_monitor_metric_alert" "memory" {
   name                = "${azurerm_container_app.main.name}-memory"
   resource_group_name = var.resource_group_name
   scopes              = [azurerm_container_app.main.id]
-  description         = "Action will be triggered when memory use is greater than ${var.alert_memory_threshold}%"
+  description         = "Action will be triggered when memory use is greater than ${var.alert_memory_threshold}% of container limit"
   window_size         = var.alert_window_size
   frequency           = local.alert_frequency
 
   criteria {
     metric_namespace = "Microsoft.App/containerApps"
-    metric_name      = "memory_percent"
+    metric_name      = "MemoryPercentage"
     aggregation      = "Average"
     operator         = "GreaterThan"
     threshold        = var.alert_memory_threshold
@@ -33,13 +33,13 @@ resource "azurerm_monitor_metric_alert" "cpu" {
   name                = "${azurerm_container_app.main.name}-cpu"
   resource_group_name = var.resource_group_name
   scopes              = [azurerm_container_app.main.id]
-  description         = "Action will be triggered when cpu use is greater than ${var.azure_cpu_threshold}%"
+  description         = "Action will be triggered when cpu use is greater than ${var.azure_cpu_threshold}% of container limit"
   window_size         = var.alert_window_size
   frequency           = local.alert_frequency
 
   criteria {
     metric_namespace = "Microsoft.App/containerApps"
-    metric_name      = "cpu_percent"
+    metric_name      = "CpuPercentage"
     aggregation      = "Average"
     operator         = "GreaterThan"
     threshold        = var.azure_cpu_threshold
@@ -56,31 +56,31 @@ resource "azurerm_monitor_metric_alert" "cpu" {
   }
 }
 
-resource "azurerm_monitor_metric_alert" "storage" {
-  count = var.enable_monitoring ? 1 : 0
+# resource "azurerm_monitor_metric_alert" "storage" {
+#   count = var.enable_monitoring ? 1 : 0
 
-  name                = "${azurerm_container_app.main.name}-storage"
-  resource_group_name = var.resource_group_name
-  scopes              = [azurerm_container_app.main.id]
-  description         = "Action will be triggered when storage use is greater than ${var.azure_storage_threshold}%"
-  window_size         = var.alert_window_size
-  frequency           = local.alert_frequency
+#   name                = "${azurerm_container_app.main.name}-storage"
+#   resource_group_name = var.resource_group_name
+#   scopes              = [azurerm_container_app.main.id]
+#   description         = "Action will be triggered when storage use is greater than ${var.azure_storage_threshold}% of container limit"
+#   window_size         = var.alert_window_size
+#   frequency           = local.alert_frequency
 
-  criteria {
-    metric_namespace = "Microsoft.App/containerApps"
-    metric_name      = "storage_percent"
-    aggregation      = "Average"
-    operator         = "GreaterThan"
-    threshold        = var.azure_storage_threshold
-  }
+#   criteria {
+#     metric_namespace = "Microsoft.App/containerApps"
+#     metric_name      = "storage_percent"
+#     aggregation      = "Average"
+#     operator         = "GreaterThan"
+#     threshold        = var.azure_storage_threshold
+#   }
 
-  action {
-    action_group_id = var.action_group_id
-  }
+#   action {
+#     action_group_id = var.action_group_id
+#   }
 
-  lifecycle {
-    ignore_changes = [
-      tags
-    ]
-  }
-}
+#   lifecycle {
+#     ignore_changes = [
+#       tags
+#     ]
+#   }
+# }
