cf043de3102a0ffdf4a7597fd5e2d9eb26e34eb7
Harriet H-W <harriethw@gmail.com>
2025-10-24 17:16:28 +0100

    Add an Alert on Exceptions for App Insights
    We want to trigger an alert whenever there is an
    exception for manage-breast-screening -
    specifically for the Invite team's container app
    jobs in the notifications app.
    
    This should trigger whenever the Exception count
    is above 0 - may want to tweak that but in theory
    there should never be an Exception unless
    something has completely failed.

--- infrastructure/modules/app-insights/variables.tf
+++ infrastructure/modules/app-insights/variables.tf
@@ -33,3 +33,30 @@ variable "resource_group_name" {
   type        = string
   description = "The name of the resource group in which the App Insights is created. Changing this forces a new resource to be created."
 }
+
+variable "enable_alerting" {
+  description = "Whether monitoring and alerting is enabled for this module."
+  type        = bool
+  default     = false
+}
+
+variable "alert_frequency" {
+  type     = string
+  nullable = true
+  default  = "PT5M"
+  validation {
+    condition     = contains(["PT1M", "PT5M", "PT15M", "PT30M", "PT1H"], var.alert_frequency)
+    error_message = "The alert_frequency must be one of: PT1M, PT5M, PT15M, PT30M, PT1H"
+  }
+  description = "The frequency an alert is checked e.g. PT1M, PT5M, PT15M, PT30M, PT1H, PT6H, PT12H."
+}
+
+variable "action_group_id" {
+  type        = string
+  description = "The ID of the Action Group to use for alerts."
+  default     = null
+}
+
+locals {
+  alert_window_size = var.alert_frequency
+}
