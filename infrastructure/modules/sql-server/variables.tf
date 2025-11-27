6144dfb45fb8eca01794bbbe686a4e9857738ec1
Maciej Murawski <114145154+MacMur85@users.noreply.github.com>
2025-11-27 11:11:35 +0000

    feat: add monitoring for db performance - alert if CPU usage above 90 percent (#263)

--- infrastructure/modules/sql-server/variables.tf
+++ infrastructure/modules/sql-server/variables.tf
@@ -264,3 +264,48 @@ variable "vulnerability_assessment_enabled" {
   description = "to enable extended auditing policy for server or database"
   default     = false
 }
+
+/* --------------------------------------------------------------------------------------------------
+  Azure Monitor Alerts variables
+-------------------------------------------------------------------------------------------------- */
+
+variable "enable_alerting" {
+  description = "Whether monitoring and alerting is enabled for the Azure SQL Server."
+  type        = bool
+  default     = false
+}
+
+variable "alert_cpu_threshold" {
+  type        = number
+  description = "If alerting is enabled this will control what the cpu threshold will be, default will be 90."
+  default     = 90
+}
+
+variable "alert_window_size" {
+  type     = string
+  nullable = false
+  default  = "PT5M"
+  validation {
+    condition     = contains(["PT1M", "PT5M", "PT15M", "PT30M", "PT1H", "PT6H", "PT12H"], var.alert_window_size)
+    error_message = "The alert_window_size must be one of: PT1M, PT5M, PT15M, PT30M, PT1H, PT6H, PT12H"
+  }
+  description = "The period of time that is used to monitor alert activity e.g. PT1M, PT5M, PT15M, PT30M, PT1H, PT6H, PT12H. The interval between checks is adjusted accordingly."
+}
+
+locals {
+  alert_frequency_map = {
+    PT5M  = "PT1M"
+    PT15M = "PT1M"
+    PT30M = "PT1M"
+    PT1H  = "PT1M"
+    PT6H  = "PT5M"
+    PT12H = "PT5M"
+  }
+  alert_frequency = local.alert_frequency_map[var.alert_window_size]
+}
+
+variable "action_group_id" {
+  type        = string
+  description = "ID of the action group to notify."
+  default     = null
+}
