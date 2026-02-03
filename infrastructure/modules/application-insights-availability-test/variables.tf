acc83924ced433327de5d2360cdd1291052ee7b2
Shahid Azim <shahid.azim2@nhs.net>
2026-02-03 12:33:18 +0000

    feat: implemented alerting for expiring certificates for application gateway

--- infrastructure/modules/application-insights-availability-test/variables.tf
+++ infrastructure/modules/application-insights-availability-test/variables.tf
@@ -54,3 +54,77 @@ variable "target_url" {
   type        = string
   description = "The target URL for the restful endpoint to hit to validate the application is available"
 }
+
+variable "http_verb" {
+  description = "HTTP verb (GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS)"
+  type        = string
+  default     = "GET"
+  validation {
+    condition     = contains(["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"], var.http_verb)
+    error_message = "http_verb must be one of GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS."
+  }
+}
+
+variable "headers" {
+  description = "Map of request headers to send (name => value)"
+  type        = map(string)
+  default     = {}
+}
+
+variable "ssl_validation" {
+  description = "SSL validation configuration for the availability test."
+  type = object({
+    expected_status_code        = optional(number, null)
+    ssl_cert_remaining_lifetime = optional(number, null)
+  })
+
+  validation {
+    condition = (
+      var.ssl_validation == null ||
+      try(var.ssl_validation.expected_status_code, 0) == 0 ||
+      (try(var.ssl_validation.expected_status_code, 0) >= 100 &&
+      try(var.ssl_validation.expected_status_code, 0) < 600)
+    )
+    error_message = "The expected status code must be 0 ('0' means 'response code < 400') or a valid HTTP status code between 100 and 599."
+  }
+
+  validation {
+    condition = (
+      var.ssl_validation == null ||
+      try(var.ssl_validation.ssl_cert_remaining_lifetime, null) == null ||
+      (try(var.ssl_validation.ssl_cert_remaining_lifetime, 0) >= 1 &&
+      try(var.ssl_validation.ssl_cert_remaining_lifetime, 0) <= 365)
+    )
+    error_message = "The SSL certificate remaining lifetime must be null or an integer between 1 and 365."
+  }
+
+  default  = null
+  nullable = true
+}
+
+variable "alert" {
+  type = object({
+    description   = optional(string, "Availability test alert")
+    frequency     = optional(string, "PT1M")
+    window_size   = optional(string, "PT5M")
+    auto_mitigate = optional(bool, true)
+  })
+
+  validation {
+    condition = contains(
+      ["PT1M", "PT5M", "PT15M", "PT30M", "PT1H"],
+      var.alert.frequency
+    )
+    error_message = "Frequency must be one of: PT1M, PT5M, PT15M, PT30M, PT1H"
+  }
+
+  validation {
+    condition = contains(
+      ["PT1M", "PT5M", "PT15M", "PT30M", "PT1H", "PT6H", "PT12H", "P1D"],
+      var.alert.window_size
+    )
+    error_message = "Window size must be one of: PT1M, PT5M, PT15M, PT30M, PT1H, PT6H, PT12H, P1D"
+  }
+
+  default = {}
+}
