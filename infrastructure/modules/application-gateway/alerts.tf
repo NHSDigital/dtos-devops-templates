65713f534f82cb3fe41d01194d08367297ca6657
Shahid Azim <shahid.azim2@nhs.net>
2026-02-06 21:48:36 +0000

    changed the kql for certified expired alert

--- infrastructure/modules/application-gateway/alerts.tf
+++ infrastructure/modules/application-gateway/alerts.tf
@@ -1,52 +1,3 @@
-resource "azurerm_monitor_scheduled_query_rules_alert_v2" "ag_certificate_near_expiry" {
-  count = var.enable_alerting == true ? 1 : 0
-
-  name                = "${azurerm_application_gateway.this.name}-certificate-near-expiry"
-  resource_group_name = var.resource_group_name_monitoring != null ? var.resource_group_name_monitoring : var.resource_group_name
-  location            = var.location
-
-  evaluation_frequency = var.certificate_near_expiry_alert.evaluation_frequency
-  window_duration      = var.certificate_near_expiry_alert.window_duration
-  scopes               = [azurerm_application_gateway.this.id]
-  severity             = 2
-
-  criteria {
-    query = <<-QUERY
-AzureDiagnostics
-| where ResourceProvider == "MICROSOFT.NETWORK"
-| where OperationName contains "CertificateNearExpiry"
-| project
-    CertificateName = column_ifexists("eventGridEventProperties_data_ObjectName_s","")
-| summarize Events=count() by CertificateName
-QUERY
-
-    time_aggregation_method = "Total"
-    threshold               = var.certificate_near_expiry_alert.threshold
-    operator                = "GreaterThanOrEqual"
-
-    resource_id_column    = "CertificateName"
-    metric_measure_column = "Events"
-
-    dimension {
-      name     = "CertificateName"
-      operator = "Include"
-      values   = ["*"]
-    }
-  }
-
-  description = "The Application Gateway certificate is nearing expiration."
-
-  action {
-    action_groups = [var.action_group_id]
-  }
-
-  lifecycle {
-    ignore_changes = [
-      tags
-    ]
-  }
-}
-
 resource "azurerm_monitor_scheduled_query_rules_alert_v2" "ag_certificate_expired" {
   count = var.enable_alerting == true ? 1 : 0
 
@@ -63,21 +14,27 @@ resource "azurerm_monitor_scheduled_query_rules_alert_v2" "ag_certificate_expire
     query = <<-QUERY
 AzureDiagnostics
 | where ResourceProvider == "MICROSOFT.NETWORK"
-| where OperationName contains "CertificateExpired"
+| where Category == "ApplicationGatewayAccessLog"
+| where OperationName contains "Ssl"
+       or OperationName contains "Certificate"
+       or Details_s contains "Certificate"
+       or failureDetails_s contains "certificate"
+| extend Status = toint(httpStatus_d)
+| where Status == 502
 | project
-    CertificateName = column_ifexists("eventGridEventProperties_data_ObjectName_s","")
-| summarize Events=count() by CertificateName
+    Details = column_ifexists("Details_s","")
+| summarize Events=count() by Details
 QUERY
 
     time_aggregation_method = "Total"
     threshold               = var.certificate_expired_alert.threshold
     operator                = "GreaterThanOrEqual"
 
-    resource_id_column    = "CertificateName"
+    resource_id_column    = "Details"
     metric_measure_column = "Events"
 
     dimension {
-      name     = "CertificateName"
+      name     = "Details"
       operator = "Include"
       values   = ["*"]
     }
